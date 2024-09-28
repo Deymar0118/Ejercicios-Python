ef87c2d1697413d8d7295234e0603282a778fdcc
Lic. Aguilar <deymar0118@gmail.com>
2024-09-27 19:06:51 -0500

    arreglo en el codigo

--- Contador.py
+++ Contador.py
@@ -3,5 +3,5 @@ Inicio = int(input("Ingrese el numero inicial: "))
 Limite = int (input("Ingrese el numero limite: "))
 
 while Inicio <= Limite:
-    print("Numero ",Inicio)
+    print("Numero: ",Inicio)
     Inicio = Inicio + 1
